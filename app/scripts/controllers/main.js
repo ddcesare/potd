3f473397002d4a038ab9a28de253bc11f67a6291
dd di cesare <didi@posteo.net>
2014-05-20 15:30:02 +0200

    using beer module (service) in controller and view

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
+
+
 angular.module('potdApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, Beer) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
 
-    
+    $scope.beer = Beer.get({id: 'random'});
 
   });
