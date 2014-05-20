20eb5a8273b96c1ec0d1a21f76b467b1bd737096
dd di cesare <didi@posteo.net>
2014-05-20 13:14:35 +0200

    first commit, yo scaffolding

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('potdApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    
+
+  });
